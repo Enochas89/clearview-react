652fac85bcd6f6ef22954c344b7a66e4bfdbeff2
Enochas89 <blitzkriegu@gmail.com>
2025-09-13 17:30:21 -0400

    Fix useState error and integrate Tailwind CSS

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Gantt from './Gantt';
 import Calendar from './Calendar';
 
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qefgcnhcrpzsphjrouha.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZmdjbmhjcnB6c3BoanJvdWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MzI1MTUsImV4cCI6MjA3MDQwODUxNX0.AE0hVIfpjqxQ8h6-L0hZO-DK2wyhiif8HovxLCC3W64';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
